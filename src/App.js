14a0070b3a54a5daa56b4dab401932f9f7616521
yjlee9909 <yjlee9909@naver.com>
2023-01-30 00:08:10 +0900

    :wrench: feat : prettier & eslint 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div className="App">hi hello</div>;
+  return (
+    <div className="App">
+      hi hello
+      <h3>hi</h3>
+    </div>
+  );
 }
 
 export default App;
