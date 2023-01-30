ada435f0dc6e13ad51caeec0adc9c4194d0ec3e5
yjlee9909 <yjlee9909@naver.com>
2023-01-30 17:08:27 +0900

    :art: feat: 글로벌 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyle";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 
 const App = () => {
   return (
     <div className="App">
+      <GlobalStyle />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
