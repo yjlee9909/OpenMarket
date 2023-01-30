7a92b5baf5e9124390f48aebaeb4437572a32380
yjlee9909 <yjlee9909@naver.com>
2023-01-30 21:47:41 +0900

    :adhesive_bandage: fix : 폰트 및 글로벌 스타일 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,31 +11,12 @@ module.exports = {
     "plugin:prettier/recommended",
   ],
   rules: {
-    "react/jsx-boolean-value": 0,
-    "react/prop-types": "off",
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
     "react/jsx-filename-extension": [
       "error",
       {
         extensions: [".js", ".jsx"],
       },
     ],
-    "react/function-component-definition": [
-      2,
-      {
-        namedComponents: "arrow-function",
-        unnamedComponents: "arrow-function",
-      },
-    ],
-    "prettier/prettier": [
-      "error",
-      {
-        singleQuote: false,
-        endOfLine: "auto",
-        trailingComma: "all",
-      },
-    ],
-    "no-console": "off",
+    "no-console": 0,
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
+import GlobalStyled from "./styles/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyled/>
     <App />
   </React.StrictMode>,
 );
