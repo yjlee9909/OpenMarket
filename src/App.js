3322f5326d585ad1b7151fece3a656faadc2800e
yjlee9909 <yjlee9909@naver.com>
2023-02-01 19:43:29 +0900

    :lipstick: style : 404 페이지 마크업

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
+import NotFound from "./pages/404/NotFound";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/notfound" element={<NotFound />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
